0ac1dd79205d5aa50343c506e69ca211f80d4529
Omar Coronado <omar.goco@gmail.com>
2022-07-06 12:59:49 -0500

    create nes users

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreatePasswordResetsTable extends Migration
@@ -14,6 +15,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
+            DB::statement('SET SESSION sql_require_primary_key=0');
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
