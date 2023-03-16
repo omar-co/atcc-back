0c027cac7b6e1325d61c7b4ed020d35e33fa3421
Francisco Guadarrama <ingfranciscoguadarrama@gmail.com>
2023-03-16 15:42:59 -0600

    Creating models for public policy

---
+++ app/Http/Controllers/PoliticaPublicaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PoliticaPublica;
+use Illuminate\Http\Request;
+
+class PoliticaPublicaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PoliticaPublica $politicaPublica)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PoliticaPublica $politicaPublica)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PoliticaPublica $politicaPublica)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PoliticaPublica  $politicaPublica
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PoliticaPublica $politicaPublica)
+    {
+        //
+    }
+}
